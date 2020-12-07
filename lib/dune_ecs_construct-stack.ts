04ced7e1edefc3ce049a284c1a11be0d888682d8
Ivan Myrvold <ivan.myrvold@abax.no>
2020-12-07 12:27:53 +0100

    dune with repository and ecs service

--- lib/dune_ecs_construct-stack.ts
+++ lib/dune_ecs_construct-stack.ts
@@ -1,9 +1,38 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
+import * as ecr from "@aws-cdk/aws-ecr"
 
 export class DuneEcsConstructStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "DuneVpc", {
+      maxAzs: 3 // Default is all AZs in region
+    });
+
+    const cluster = new ecs.Cluster(this, "DuneCluster", {
+      vpc: vpc
+    });
+
+    const repository = new ecr.Repository(this, "dune-repository", {
+      repositoryName: "dune"
+    })
+
+        // Create a load-balanced Fargate service and make it public
+        new ecs_patterns.ApplicationLoadBalancedFargateService(this, "DuneFargateService", {
+          serviceName: "DuneService",
+          cluster: cluster, // Required
+          cpu: 256, // Default is 256
+          desiredCount: 1, // Default is 1
+          taskImageOptions: { 
+            image: ecs.ContainerImage.fromEcrRepository(repository),
+            containerPort: 8080
+          },
+          memoryLimitMiB: 512, // Default is 512
+          publicLoadBalancer: true // Default is false
+        });
+    
   }
 }
